0c7b46c95aff3aca3aa8ea02576a830984172882
Eric Franck <ericjfranck@gmail.com>
2014-10-29 14:13:38 -0600

    New layout w/o gallery

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,29 @@
-var vph = 0
+var vph = 0;
+var headheight = 0;
 
 $(document).ready(function(){
     vph = $(window).height();
+    headheight = $('header').outerHeight();
+    console.log(headheight);
+    $('#title').css({
+        marginTop: vph/2-headheight/2
+    });
+    
+    $('header, section').css('minHeight', vph);
     //intro();		
+    $('#gallery li').click(function(){
+        $(this).addClass('active');
+    });
+    
+    //close modals
+    $('.close').click(function(e){
+        $(this).parent().removeClass('active');
+        e.stopPropagation();
+    });
+    
+    $(document).keyup(function(e) {
+      if (e.keyCode == 27) { 
+          $('#gallery li').removeClass('active');
+      }   // esc
+    });
 });
-
-function intro(){
-    //set initial state 
-    $('#title').css('marginTop',vph/2-90);
-    $('#title').css('display','none');
-    $('#content').css('display','none');
-    open();
-}
-
-function open(){
-    $('#title').fadeIn( "slow", function() {
-        marginTop = '0';
-        if (vph > 700){marginTop = '20%'}
-        $('#title').animate({'marginTop':marginTop}, 500, function(){
-            $('#content').fadeIn( "fast");
-        });    
-  });    
-}
