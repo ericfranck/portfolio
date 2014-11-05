757870f0e0950bf5f07698261805afb6857e5024
Eric Franck <ericjfranck@gmail.com>
2014-11-05 12:59:51 -0700

    To push to remote

--- js/custom.js
+++ js/custom.js
@@ -2,34 +2,20 @@ var vph = 0;
 var headheight = 0;
 
 $(document).ready(function(){
+    
     vph = $(window).height();
     headheight = $('header').outerHeight();
-    console.log(headheight);
     
     $('#content a').fluidbox();
     
     $('#down-arrow').click(function(e){
             $('body, html').animate({scrollTop:$('#content').offset().top - 100 + 'px'}, '700', 'swing');
     });
-    
-    
+      
     $('#title').css({
         marginTop: vph/2-headheight/3
     });
     
     $('header').css('minHeight', vph);
-    //intro();		
 
-    
-    //close modals
-    $('.close').click(function(e){
-        $(this).parent().removeClass('active');
-        e.stopPropagation();
-    });
-    
-    $(document).keyup(function(e) {
-      if (e.keyCode == 27) { 
-          $('#gallery li').removeClass('active');
-      }   // esc
-    });
 });
